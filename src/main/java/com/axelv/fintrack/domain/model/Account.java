b7536c197b66430f26b30fdb3ea17d17c1e8a16d
axelvrgn <vergognan@et.intechinfo.fr>
2026-08-21 15:09:52 +0200

    feature/init_domain_entities: init base models

--- src/main/java/com/axelv/fintrack/domain/model/Account.java
+++ src/main/java/com/axelv/fintrack/domain/model/Account.java
@@ -6,10 +6,24 @@ import java.time.LocalDateTime;
 
 public class Account {
 
+    private static final String DEFAULT_CURRENCY = "EUR";
+
     private Long id;
     private String name;
     private AccountType type;
     private Money balance;
     private LocalDateTime createdAt;
 
+
+    public Account(Long id, String name, AccountType type, Money balance, LocalDateTime createdAt) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.balance = balance;
+        this.createdAt = createdAt;
+    }
+
+    public static Account create(String name, AccountType type) {
+        return new Account(null, name, type, Money.zero(DEFAULT_CURRENCY), LocalDateTime.now());
+    }
 }
---
+++ src/main/java/com/axelv/fintrack/domain/model/enums/TransactionType.java
@@ -0,0 +1,6 @@
+package com.axelv.fintrack.domain.model.enums;
+
+public enum TransactionType {
+    CREDIT,
+    DEBIT
+}
